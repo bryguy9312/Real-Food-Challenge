b09a03309b4cf801c0508d05a444e11d56d1e6e5
Taurvi <srimbakusumo@gmail.com>
2015-07-18 12:19:07 -0700

    More notes, more debug code

--- Notes/testcode/js/main.js
+++ Notes/testcode/js/main.js
@@ -1,11 +1,18 @@
 //Debug Functions
 var debugMode = true;
+var debugVars = {};
 
 var debugMsg = function(msg) {
     if (debugMode)
         console.log('<<<|DEBUG|>>> ' + msg);
 }
 
+//Asynchronous function, all manipulation should occur in here.
 d3.json("data/data.json", function(error, data){
-    //Asynchronous function, all manipulation should occur in here.
+    debugVars.data = data;
+    debugMsg('Data Loaded!')
 });
+
+var dataCategorize =  function(data) {
+    data.map()
+}
