149ef4d9c9cf8db340bfc052ddb72846919f1b4d
Taurvi <srimbakusumo@gmail.com>
2015-07-22 11:07:42 -0700

    Fixed data manipulation.

--- js/discrete_d3_BarGraph.js
+++ js/discrete_d3_BarGraph.js
@@ -1,145 +1,220 @@
-"use strict";
+//Debug Functions
+var debugMode = true;
+var debugVars = {};
 
-
-var data;
-var BarGraph = function(settings){
-    var self = this;
-    self.defaults = {
-        margin: {
-            left:50,
-            bottom: 50,
-            top: 50,
-            right: 50
-        },
-        xVar: 'category',
-        yVar: 'rfp',
-        hoverTitle: 'rfp(%):',
-        id: "svg1",
-        totalHeight: 600,
-        totalWidth: 600
-    };
-    self.settings = $.extend(false, self.defaults, settings);
-    self.settings.height = self.settings.totalHeight - self.settings.margin.bottom - self.settings.margin.top;
-    self.settings.width  = self.settings.totalWidth - self.settings.margin.left - self.settings.margin.right;
-    self.rectFunc = function(rect) {
-        debugMsg('=======rectFunc executing=======');
-        debugMsg('self.width: ' + self.settings.width);
-        debugMsg('self.index: ' + self.settings.index);
-        debugMsg('self.margin.right: ' + self.settings.margin.right);
-        console.log(self.settings.data);
-        console.log(rect);
-
-        rect.attr('width', self.xScale(20))
-            .attr('height', function(d) {
-                return self.settings.height - self.yScale(d[self.settings.yVar])
-            })
-            .attr('fill', '#D74523')
-            .attr('x', function(d, index) {
-                return  (self.xScale((index * 30)));
-            })
-            .attr('y', function(d) {
-                return (self.yScale(d[self.settings.yVar]));
-            })
-            .attr('title', function(d) {
-                debugMsg('rectFunc title');
-                 return self.settings.hoverTitle + d[self.settings.yVar];
-            });
-        debugMsg('=======rectFunc ending=======')
-    };
-
-    self.build();
+var debugMsg = function(msg) {
+    if (debugMode)
+        console.log('<<<|DEBUG|>>> ' + msg);
 };
 
-BarGraph.prototype.setScales = function() {
-    debugMsg('setting scales');
-    var self = this;
-    var yMin = 0;
-    var yMax = d3.max(self.settings.data, function(d) { return (d[self.settings.yVar])});
-
-    self.xScale = d3.scale.linear().range([0, self.settings.width]).domain([0, self.settings.width]);
-    self.yScale = d3.scale.linear().range([self.settings.height, 0]).domain([yMin, yMax]);
-    debugVars.yScale = self.yScale;
-    self.xAxis = d3.svg.axis().scale(self.xScale).orient('bottom').ticks(0);
-    self.yAxis = d3.svg.axis().scale(self.yScale).orient('left');
-    debugMsg('scales set');
-};
-
-BarGraph.prototype.build = function() {
-    debugMsg('building');
-    var self = this;
-    //creates a 400x400 svg in the body, this will need to be modified to fit in the final html
-    self.svg = d3.select('body').append('svg').attr('height', self.settings.totalHeight).attr('width', self.settings.totalWidth).attr('id', this.settings.id);
-
-    //G for bars
-    self.g = self.svg.append('g')
-        .attr('transform', 'translate(' + self.settings.margin.left + ',' + self.settings.margin.top + ')')
-        .attr('height', self.settings.height)
-        .attr('width', self.settings.width);
-
-    //G for x axis
-    self.xAxisG = self.svg.append('g')
-        .attr('transform', 'translate(' + self.settings.margin.left + ',' + (self.settings.height + self.settings.margin.top) + ')')
-        .attr('class', 'axis');
-
-    //G for y axis
-    self.yAxisG = self.svg.append('g')
-        .attr('transform', 'translate(' + self.settings.margin.left + ',' + (self.settings.margin.top) + ')')
-        .attr('class', 'axis');
-
-    self.draw();
-    self.addHover();
-    debugMsg('building done');
-};
-
-BarGraph.prototype.draw = function() {
-    debugMsg('drawing');
-    var self = this;
-    self.setScales();
-    // bind self.settings.data
-    debugMsg('binding data to rectangles');
-    var bars = self.g.selectAll('rect').data(self.settings.data);
-    debugMsg("data is bound to rectangles, logging bound data");
-    //enter new elements
-    bars.enter().append('rect').call(self.rectFunc);
-    // exit elements that may have left
-    bars.exit().remove();
-    //transition all rects to new self.settings.data??
-    self.g.selectAll('rect').transition().duration(1500).call(self.rectFunc);
-
-    self.xAxisG.call(self.xAxis);
-    self.yAxisG.call(self.yAxis);
-    debugMsg('drawing done');
-    debugVars.self = self;
-
-    self.svg.selectAll('.bartext').data(self.settings.data).enter().append('text')
-        .attr('class', 'bartext')
-        .attr('text-anchor', 'middle')
-        .attr('x', function(d, i) { return self.xScale(i*50)})
-        .attr('y', self.settings.totalHeight - 10)
-        .text(function(d) {return d.category});
+//Asynchronous function, all manipulation should occur in here.
+// !!! IMPORTANT !!! formattedData is the data formatted exactly to specification.
+debugMsg('Data Loading!');
+d3.text("data/data.csv", function(data) {
+    var rawData = d3.csv.parseRows(data);
+    debugVars.data = setupData(rawData);
+});
 
 
-    self.svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "middle")
-        .attr("x", self.settings.width / 2)
-        .attr("y", self.settings.totalHeight-15)
-        .text(self.settings.xVar);
-    self.svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "middle")
-        .attr("y", 5)
-        .attr("x", - self.settings.totalHeight/2)
-        .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
-        .text(self.settings.yVar);
-};
+/*
+ ********************************
+ * DATA MANIPULATION FUNCTIONS  *
+ ********************************
+ */
 
-BarGraph.prototype.addHover = function() {
-    $('rect').tooltip({
-        'container': 'body',
-        'placement': 'bottom'
+var setupData = function(rawData) {
+    transformedData = dataTransform(rawData);
+    debugVars.transformedData = transformedData;
+    var categorized = dataCategorize(transformedData);
+    var formattedData = initData(categorized);
+    var categoryResults = {};
+    // Loops through each of the categories and breaks them down into the four components of real food.
+
+    for(var i = 0; i < formattedData.length; i++) {
+        if(formattedData[i].category != 'total') {
+            var category = formattedData[i].category;
+            var realFoodData = realData(categorized[category]);
+            formattedData[i]['categories'] = realFoodData;
+        } else {
+            var totalRealFood = realData(transformedData);
+            formattedData[i]['categories'] = totalRealFood;
+        }
+    }
+    debugVars.formattedData = formattedData;
+    return formattedData;
+}
+// Transforms the data from CSV to the JSON-esque format.
+var dataTransform = function(rawData) {
+    debugMsg('Data Transforming!');
+    var key;
+    var transformedData = [];
+    rawData.map(function(rawRow) {
+        // Sets the key
+        if(!key)
+            key = rawRow;
+        else {
+            if(checkValid(rawRow)) {
+                var transformedRow = {};
+                // Iterate through the row data;
+                for(var i = 0; i < rawRow.length; i++) {
+                    var fieldTitle = key[i];
+                    // Checks if the category doesn't exist, then labels it "uncategorized"
+                    if(key[i] == 'Category' && rawRow[i] == '')
+                        rawRow[i] = 'uncategorized';
+                    transformedRow[fieldTitle] = rawRow[i];
+                }
+                transformedData.push(transformedRow);
+            }
+        }
     });
+    debugMsg('Data Transformed!');
+    debugVars.key = key;
+    return transformedData;
 };
 
+/**
+ * Removes any entries that do not have a name or cost.
+ */
+var checkValid = function(rawRow) {
+    valid = true;
+    // rawRow[0] = item name; rawRow[11] = item cost
+    if (rawRow[0] == '' || rawRow[11] == '') {
+        valid = false;
+    }
+    if (valid == false)
+        debugMsg("Entry has failed the test: " + rawRow);
+    return valid;
+}
 
+/**
+ * Organizes the raw data into categories.
+ * @param {Object[]} data - The raw data from the json file.
+ * @return {Object[]} categorized - The data sorted into categories.
+ */
+var dataCategorize =  function(data) {
+    debugMsg('Categorizing data!');
+    var categorized = {};
+    data.map(function(entry){
+        var category = entry.Category;
+        if(!categorized.hasOwnProperty(category))
+            categorized[category] = [];
+        categorized[category].push(entry);
+    });
+    debugMsg('Data categorized!');
+    debugVars.categorized = categorized;
+    return categorized;
+};
+
+/**
+ * Creates the data for Real Food Value.
+ * @param {Object[]} data - The categorized data.
+ * @return {Object[]} initialData - Calculated Real Food Value totals.
+ */
+var initData = function(categorizedData) {
+    debugMsg('Creating initial data!');
+    var initialData = [];
+    // Iterates through each category
+    var totalRealFood = 0;
+    var totalFakeFood = 0;
+    for (var category in categorizedData) {
+        // Checks that the key exists in the data set
+        if (categorizedData.hasOwnProperty(category)) {
+            var realFood = 0;
+            var fakeFood = 0;
+            // Iterates through each food item
+            categorizedData[category].map(function(food) {
+                if(checkRealFood(food)) {
+                    realFood += parseFloat(food.Cost);
+                    totalRealFood += parseFloat(food.Cost);
+                } else {
+                    fakeFood += parseFloat(food.Cost);
+                    totalFakeFood += parseFloat(food.Cost);
+                }
+            });
+            initialData.push({
+                'category' : category,
+                'realFood' : realFood,
+                'fakeFood' : fakeFood,
+                'totalFood' : realFood + fakeFood,
+                'rfp' : (realFood / (realFood + fakeFood)) * 100
+            })
+        } else
+            debugMsg("Execution error! Unidentified key passed!")
+    }
+    initialData.push({
+        'category': 'total',
+        'realFood': totalRealFood,
+        'fakeFood': totalFakeFood,
+        'totalFood': totalRealFood + totalFakeFood,
+        'rfp': (totalRealFood / (totalRealFood + totalFakeFood)) * 100
+    });
+    debugMsg('Initial data created!');
+    debugVars.initialData = initialData;
+    return initialData;
+};
+
+/**
+ * Breaks down real food into the four categories (Local, Fair, Ecological, Humane).
+ * NOTE: Duplicates are added, so there is a chance that you will go over 100%. Still pending fix.
+ * @param {Object[]} category - An organized category.
+ * @return {Object[]} categorized - The total amount of real food divided into the categories.
+ */
+var realData = function(category) {
+    //debugMsg('Creating real data!');
+    var realFood = {
+        'local' : 0,
+        'fair' : 0,
+        'ecological' : 0,
+        'humane': 0,
+        'total' : 0
+    };
+    var realFood = [
+        {
+            'category': 'ecological',
+            'cost' : 0
+        },
+        {
+            'category': 'fair',
+            'cost' : 0
+        },
+        {
+            'category': 'humane',
+            'cost' : 0
+        },
+        {
+            'category': 'local',
+            'cost' : 0
+        },
+        {
+            'category': 'total',
+            'cost' : 0
+        }
+    ]
+    category.map(function(food) {
+        if(checkRealFood(food)) {
+            if(food.Ecological == 'yes')
+                realFood[0].cost += parseFloat(food.Cost);
+            if(food.Fair == 'yes')
+                realFood[1].cost += parseFloat(food.Cost);
+            if(food.Humane == 'yes')
+                realFood[2].cost += parseFloat(food.Cost);
+            if(food.Local == 'yes')
+                realFood[3].cost += parseFloat(food.Cost);
+            realFood[4].cost += parseFloat(food.Cost);
+        }
+    });
+    debugVars.realFood = realFood;
+    //debugMsg('Real data created!');
+    return realFood;
+};
+
+/**
+ * Creates the data for Real Food Value.
+ * @param {Object[]} data - The categorized data.
+ * @return {Object[]} initialData - Calculated Real Food Value totals.
+ */
+var checkRealFood = function(entry) {
+    if (entry.Disqualifier == 'yes')
+        return false;
+    return (entry.Local == 'yes' || entry.Fair == 'yes' || entry.Ecological == 'yes' || entry.Humane == 'yes');
+};
